02da79549faa0cf23b90d285bfa866adc9b7c8f2
Kenny <kenny@yeoyou.net>
2019-02-05 00:55:01 +1100

    remove navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
-const NavVar = () => <div>NavVar</div>;
 const Products = () => <div>Products</div>;
 
 class App extends Component {
   render() {
     return (
       <>
-        <NavVar />
         <Router>
           <Switch>
             <Route path="/" exact component={() => <Redirect to="/products" />} />
