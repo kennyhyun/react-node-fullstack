4cc760cb23e2749c28682219f8c7421f4649aa08
Kenny Yeo <kenny@yeoyou.net>
2019-02-04 02:24:07 +0000

    init backend with express-restify-mongoose

---
+++ restapi/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const restify = require('express-restify-mongoose')
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const app = express()
+const router = express.Router()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(methodOverride())
+
+const {
+  MONGODB = 'mongodb://localhost:27017/database',
+} = process.env;
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(MONGODB)
+    .then(() => {
+      console.log('MongoDB connected…');
+      // dummyData();
+    })
+    .catch(err => console.log(err));
+}
+
+app.use('/', indexRouter);
+
+restify.serve(router, mongoose.model('Product', new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String }
+})))
+
+app.use(router)
+
+module.exports = app;
