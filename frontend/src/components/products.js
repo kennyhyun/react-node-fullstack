39747b68f1492cebc427cba5623e8a8fc1929efd
Kenny <kenny@yeoyou.net>
2019-02-05 16:39:01 +1100

    frontend/responsive

--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -2,8 +2,10 @@ import React from 'react';
 import injectSheet from 'react-jss'
 import { connect } from 'react-redux';
 import { compose, lifecycle } from 'recompose';
+import withSizes from 'react-sizes';
 
 import { ProductCard, ListHeader, ListFooter } from 'react-components';
+import Grid from '@material-ui/core/Grid';
 
 import * as actions from '../actions/product';
 
@@ -16,31 +18,38 @@ const styles = {
       width: '40%',
     },
   },
+  wrapper: {
+    overflow: 'hidden',
+  },
 };
 
-const Products = ({ appState, state, classes }) => {
-  console.log(state, appState);
+const Products = ({ appState, state, classes, isMobile, setPage }) => {
   const { page, products: allProducts } = state;
   const { items } = appState;
   const total = allProducts.length;
-  // const pages = Math.floor(allProducts.length / items) + 1;
   const offset = ((page - 1) * items);
   const products = allProducts.slice(offset, offset + items);
+
   return (
   <div className={classes.wrapper}>
-    <ListHeader title="All Products" />
-    <div className={classes.grid}>
+    <ListHeader
+      title="All Products"
+      totalItems={total}
+    />
+    <Grid container spacing={isMobile ? 8 : 16}>
       {products.map((p, i) => {
-        return <ProductCard
-            key={p._id}
+        return <Grid item xs={6} sm={4} md={3} lg={2} key={p._id}>
+          <ProductCard
             title={p.name}
             description={p.description}
             price={p.price}
-          />;
+            image={p.image}
+          />
+        </Grid>;
       })}
-    </div>
+    </Grid>
     <ListFooter
-      confirmPage={p => console.log(p)}
+      confirmPage={setPage}
       page={page}
       itemsPerPage={items}
       totalItems={total}
@@ -56,6 +65,9 @@ export default compose(
       appState: state.appReducer,
     }),
     dispatch => ({
+      setPage(p) {
+        return dispatch(actions.setPage(p));
+      },
       fetchProducts() {
         return dispatch(actions.fetchProducts());
       },
@@ -66,6 +78,9 @@ export default compose(
       this.props.fetchProducts();
     },
   }),
+  withSizes(({ width }) => ({
+    isMobile: width < 600,
+  })),
   injectSheet(styles)
 )(Products);
 
