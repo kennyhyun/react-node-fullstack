2c6bcb526ac166562c7cd829d56591998a8a54a7
Kenny <kenny@yeoyou.net>
2019-02-05 11:34:58 +1100

    render product page with dummies

--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import injectSheet from 'react-jss'
 
-import { ListHeader } from 'react-components';
+import { ProductCard, ListHeader, ListFooter } from 'react-components';
 
-const ListFooter = ({ page }) => <div>{`page ${page}`}</div>;
+const styles = {
+  grid: {
+    display: 'flex',
+    margin: [[ 16, -16 ]],
+    '& > *': {
+      margin: 16,
+      width: '40%',
+    },
+  },
+};
 
-const Products = ({ page }) => (
-  <>
+const Products = ({ page, classes }) => (
+  <div className={classes.wrapper}>
     <ListHeader title="All Products" />
-    <div>
-      list
+    <div className={classes.grid}>
+      {Array(4).fill(null).map((_, i) => {
+        return <ProductCard
+            key={i}
+            title={`title ${i}`}
+            description={`description ${i}`}
+            price={`$${i}.99`}
+          />;
+      })}
     </div>
     <ListFooter page={page} />
-  </>
+  </div>
 );
 
-export default Products;
+export default injectSheet(styles)(Products);
 
