44aa4e01ff722674057c3cb6743f95ea8d7e3558
Kenny Yeo <kenny@yeoyou.net>
2019-02-06 21:33:52 +0000

    frontend/update env var

--- frontend/src/utils/apiCall.js
+++ frontend/src/utils/apiCall.js
@@ -1,11 +1,12 @@
 import fetch from 'isomorphic-fetch';
 
 const {
-  BASE_URL = '/api/v1',
+  REACT_APP_API_HOST = '',
+  REACT_APP_BASE_URL = '/api/v1',
 } = process.env;
 
 export const apiCall = (endpoint, method = 'get', body) =>
-  fetch(`${BASE_URL}/${endpoint}`, {
+  fetch(`${REACT_APP_API_HOST}${REACT_APP_BASE_URL}/${endpoint}`, {
     headers: { 'content-type': 'application/json' },
     method,
     body: JSON.stringify(body),
